ba9c7ab7107eb3fb16d53754b1daaa336fc02129
amna <amna.ejaz488@gmail.com>
2025-04-20 04:55:59 -0400

    added script

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  envDir: './env',
-  plugins: [react()],
+    plugins: [react()],
+    server: {
+        port: 3000, // Optional: Specify the port for local development
+    },
+    build: {
+        outDir: 'dist', // Default output directory
+    },
 });
